714f4c58a490b45255aadc3c248a694b7c683ab2
tmnrwd <61465712+tmnrwd@users.noreply.github.com>
2020-10-05 12:33:25 +0100

    Updates

---
+++ precache-manifest.036074fc25abf4e880879c707034583d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb888254b815d4c8440c144f620524bf",
+    "url": "/shopping-list-app/index.html"
+  },
+  {
+    "revision": "19b464b9282bc1ed1d2b",
+    "url": "/shopping-list-app/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "19b464b9282bc1ed1d2b",
+    "url": "/shopping-list-app/static/js/2.c35b4914.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/shopping-list-app/static/js/2.c35b4914.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85a77d8d69372542a06f",
+    "url": "/shopping-list-app/static/js/main.2737685d.chunk.js"
+  },
+  {
+    "revision": "0f4882acfc29aa9bbd26",
+    "url": "/shopping-list-app/static/js/runtime-main.664eb4c5.js"
+  }
+]);
